a5ae17b909020980f7d1cbf6d1fe365d3654c054
Jonathan White <jawh@umich.edu>
2022-07-02 19:37:11 -0600

    Extended performance tests to include insertion at front and increased granularity of large node sizes.

--- vector_list_performance_test.cpp
+++ vector_list_performance_test.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include <iostream>
 #include <list>
+#include <numeric>
 #include <random>
 #include <string>
 #include <vector>
@@ -43,8 +44,19 @@ static auto copy_sort(const C& container) -> C
     return copy;
 }
 
+template <typename C>
+static auto copy_reverse(const C& container) -> C
+{
+    auto copy = C{};
+    for (auto x : container)
+    {
+        copy.insert(copy.begin(), x);
+    }
+    return copy;
+}
+
 template <int NodeSize>
-static void BM_vector_insert(benchmark::State& state)
+static void BM_vector_random_insert(benchmark::State& state)
 {
     auto random_vector = gen_random_data<std::vector<Node<NodeSize>>>(state.range(0));
     for (auto _ : state)
@@ -57,13 +69,36 @@ static void BM_vector_insert(benchmark::State& state)
     state.counters["items"] = state.range(0);
     state.counters["item_size"] = sizeof(Node<NodeSize>);
 }
-BENCHMARK_TEMPLATE(BM_vector_insert, 1)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE(BM_vector_insert, 8)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE(BM_vector_insert, 64)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE(BM_vector_insert, 512)->Range(4, 1<<20);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 1)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 8)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 64)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 128)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 256)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_random_insert, 512)->Range(4, 1<<18);
 
 template <int NodeSize>
-static void BM_list_insert(benchmark::State& state)
+static void BM_vector_front_insert(benchmark::State& state)
+{
+    auto random_vector = gen_random_data<std::vector<Node<NodeSize>>>(state.range(0));
+    for (auto _ : state)
+    {
+        auto copy = std::vector<Node<NodeSize>>{};
+        benchmark::DoNotOptimize(copy = copy_reverse(random_vector));
+    }
+    state.SetItemsProcessed(state.range(0));
+    state.SetBytesProcessed(state.range(0)*sizeof(Node<NodeSize>));
+    state.counters["items"] = state.range(0);
+    state.counters["item_size"] = sizeof(Node<NodeSize>);
+}
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 1)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 8)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 64)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 128)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 256)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_vector_front_insert, 512)->Range(4, 1<<18);
+
+template <int NodeSize>
+static void BM_list_random_insert(benchmark::State& state)
 {
     auto random_list = gen_random_data<std::list<Node<NodeSize>>>(state.range(0));
     for (auto _ : state)
@@ -76,13 +111,32 @@ static void BM_list_insert(benchmark::State& state)
     state.counters["items"] = state.range(0);
     state.counters["item_size"] = sizeof(Node<NodeSize>);
 }
-BENCHMARK_TEMPLATE1(BM_list_insert, 1)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE1(BM_list_insert, 8)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE1(BM_list_insert, 64)->Range(4, 1<<20);
-BENCHMARK_TEMPLATE1(BM_list_insert, 512)->Range(4, 1<<20);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 1)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 8)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 64)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 128)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 256)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE1(BM_list_random_insert, 512)->Range(4, 1<<18);
+
+template <int NodeSize>
+static void BM_list_front_insert(benchmark::State& state)
+{
+    auto random_list = gen_random_data<std::list<Node<NodeSize>>>(state.range(0));
+    for (auto _ : state)
+    {
+        auto copy = std::list<Node<NodeSize>>{};
+        benchmark::DoNotOptimize(copy = copy_reverse(random_list));
+    }
+    state.SetItemsProcessed(state.range(0));
+    state.SetBytesProcessed(state.range(0)*sizeof(Node<NodeSize>));
+    state.counters["items"] = state.range(0);
+    state.counters["item_size"] = sizeof(Node<NodeSize>);
+}
+BENCHMARK_TEMPLATE(BM_list_front_insert, 1)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_list_front_insert, 8)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_list_front_insert, 64)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_list_front_insert, 128)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_list_front_insert, 256)->Range(4, 1<<18);
+BENCHMARK_TEMPLATE(BM_list_front_insert, 512)->Range(4, 1<<18);
 
 BENCHMARK_MAIN();
-//int main()
-//{
-//    for (auto i : gen_random_data<std::vector<int>>(8)) { std::cout << i << '\n'; }
-//}
